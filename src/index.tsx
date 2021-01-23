fa675c4a0ec19bd981775e90bac45d775f667915
Noel Kirkland <noelkirkland@gmail.com>
2021-01-22 20:51:48 -0800

    add code transpiler

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,32 @@
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => {
-    console.log(input)
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+    setCode(result.code)
   }
   return <div>
     <textarea
@@ -16,7 +36,7 @@ const App = () => {
       <button
         onClick={onClick}>Submit</button>
     </div>
-    <pre></pre>
+    <pre>{code}</pre>
   </div>;
 };
 
