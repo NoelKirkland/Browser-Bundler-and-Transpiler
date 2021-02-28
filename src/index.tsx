f823f152d7589703e6a08f75b1f628d76fe30b9d
Noel Kirkland <noelkirkland@gmail.com>
2021-02-27 19:19:08 -0800

    seperate plugins int unpkg and fetch

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as esbuild from 'esbuild-wasm';
-import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -11,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: '/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -27,26 +28,28 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()]
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
-    // console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
 
-    setCode(result.outputFiles[0].text)
-  }
-  return <div>
-    <textarea
-      value={input}
-      onChange={e => setInput(e.target.value)}></textarea>
+  return (
     <div>
-      <button
-        onClick={onClick}>Submit</button>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
-    <pre>{code}</pre>
-  </div>;
+  );
 };
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
